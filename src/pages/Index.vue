813715c8d4da7b2352fa174d79ff97c77ce424cf
chen-GM <153960756@qq.com>
2020-09-02 20:06:05 +0800

    first

---
+++ src/pages/Index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+        首页
+
+        <el-row justify="center" type="flex" :gutter="10">
+            <el-col :span="11">
+                <div class="box">11</div>
+            </el-col>
+            <el-col :span="11">
+                <div class="box">11</div>
+            </el-col>
+
+        </el-row>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    .container{
+        width: 80%;
+        margin: 0 auto;
+        border: 1px solid #ccc;
+    }
+    .box{
+        background: pink;
+        border: 1px solid red;
+    }
+</style>
